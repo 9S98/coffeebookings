cde2e42f8f63111a3fd1423d5dcfb5fc93a6063b
Sawah Abdullah <9saw98@gmail.com>
2025-06-01 02:22:57 +0000

    here

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,7 @@ const nextConfig: NextConfig = {
    */
   allowedDevOrigins: [
     'https://6000-firebase-studio-1748457547063.cluster-oayqgyglpfgseqclbygurw4xd4.cloudworkstations.dev',
+    'https://9000-firebase-studio-1748457547063.cluster-oayqgyglpfgseqclbygurw4xd4.cloudworkstations.dev',
   ],
 };
 
